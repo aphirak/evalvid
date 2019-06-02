0c873768d07d6a21262652c7126b97550248aeea
lsiddd <lucassidpacheco@gmail.com>
2019-06-02 18:59:29 -0300

    more bug fixes

--- metricas.sh
+++ metricas.sh
@@ -1,57 +1,54 @@
 #!/bin/bash
 
-here=$(pwd)
-video=$4
-
 if [ $1 == "-c" ]
 then
     rm Video* loss* rate* delay* rd* sd*
     exit 0
 fi
 
-if [ "$video" == "container" ]
+if [ $4 == "container" ]
 then
     video_st="st_container_cif_h264_300_20.st"
     video_yuv="container_cif_h264_300_20"
     yres=288
     xres=352
 
-elif [ $video == "highway" ]
+elif [ $4 == "highway" ]
 then
     video_st="st_highway_cif.st"
     video_yuv="highway_cif"
     yres=288
     xres=352
 
-elif [ $video == "highway600" ]
+elif [ $4 == "highway600" ]
 then
     video_st="st_highway_600_cif"
     video_yuv="highway_600_cif"
     yres=288
     xres=352
 
-elif [ $video == "akiyo" ]
+elif [ $4 == "akiyo" ]
 then
     video_st="st_akiyo_cif_h264_300_18"
     video_yuv="akiyo_cif_h264_300_20"
     yres=288
     xres=352
 
-elif [ $video == "masha" ]
+elif [ $4 == "masha" ]
 then
     video_st="st_masha360p"
     video_yuv="masha360p_ref"
     yres=360
     xres=640
 
-elif [ $video == "despacito" ]
+elif [ $4 == "despacito" ]
 then
     video_st="st_despacito"
     video_yuv="despacito_ref"
     yres=360
     xres=640
 
-elif [ $video == "babyshark" ]
+elif [ $4 == "babyshark" ]
 then
     video_st="st_baby_shark"
     video_yuv="baby_shark_ref"
@@ -64,13 +61,13 @@ fi
 
 for i in $1; do
 	for k in $(seq 1 $2); do
-		cp $here/$i/simul$k/rd* $here/$i/simul$k/sd* "$here"
+		cp $i/simul$k/rd* $i/simul$k/sd* "$here"
 		for y in $(seq 0 $3); do
-			 $here/etmp4 -F -0 sd_a01_$y rd_a01_$y "$here/traces/$video_st" "$here/reference_videos/$video_yuv.mp4" Video$y
+			 etmp4 -F -0 sd_a01_$y rd_a01_$y "traces/$video_st" "reference_videos/$video_yuv.mp4" Video$y
 			 ffmpeg -i Video$y.mp4 Video$y.yuv
-			 wine $here/msu_metric.exe -f "$here/reference_videos/$video_yuv.yuv" IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr ssim_precise -cc YYUV
-			 wine $here/msu_metric.exe -f "$here/reference_videos/$video_yuv.yuv" IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr vqm -cc YYUV
-			 mv *csv loss* delay* rate* Video*.mp4  $i/simul$k/
+			 wine msu_metric.exe -f reference_videos/$video_yuv.yuv IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr ssim_precise -cc YYUV
+			 wine msu_metric.exe -f reference_videos/$video_yuv.yuv IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr vqm -cc YYUV
+			 mv reference_videos/*csv loss* delay* rate* Video*.mp4  $i/simul$k/
              rm *.yuv
 		done
 
