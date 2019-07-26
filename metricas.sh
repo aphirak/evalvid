3e1549fcc19c33fa5cda38a60426ca641098b70f
lsiddd <lucassidpacheco@gmail.com>
2019-07-26 16:29:16 -0300

    metricas script updated

--- metricas.sh
+++ metricas.sh
@@ -59,16 +59,27 @@ else
     exit 1
 fi
 
+echo "executing for video ${4}"
+echo "st: ${video_st}"
+
 for i in $1; do
 	for k in $(seq 1 $2); do
 		cp $i/simul$k/rd* $i/simul$k/sd* .
+        echo status of copying files: $?
 		for y in $(seq 0 $3); do
-			 ./etmp4 -F -0 sd_a01_$y rd_a01_$y "traces/$video_st" "reference_videos/$video_yuv.mp4" Video$y
-			 ffmpeg -i Video$y.mp4 Video$y.yuv
-			 wine msu_metric.exe -f reference_videos/$video_yuv.yuv IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr ssim_precise -cc YYUV
-			 wine msu_metric.exe -f reference_videos/$video_yuv.yuv IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr vqm -cc YYUV
-			 mv reference_videos/*csv loss* delay* rate* Video*.mp4  $i/simul$k/
+			 ./etmp4 -F -0 sd_a01_$y rd_a01_$y "traces/$video_st" "reference_videos/$video_yuv.mp4" Video$y > /dev/null 2>&1
+             echo mp4 generated with status $?
+
+			 ffmpeg -i Video$y.mp4 Video$y.yuv > /dev/null 2>&1
+             echo ffmpeg status: $?
+
+			 wine msu_metric.exe -f reference_videos/$video_yuv.yuv IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr ssim_precise -cc YYUV > /dev/null 2>&1
+			 wine msu_metric.exe -f reference_videos/$video_yuv.yuv IYUV -yw $xres -yh $yres -f Video$y.yuv -sc 1 -cod . -metr vqm -cc YYUV > /dev/null 2>&1
+             echo wine msu_metrics status: $?
+
+			 mv reference_videos/*csv loss* delay* rate* Video*.mp4  $i/simul$k/ > /dev/null 2>&1
              rm *.yuv
+             echo
 		done
 	done
 done
